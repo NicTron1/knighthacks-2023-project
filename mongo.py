7f9d6edbb80e316aa247928aaa5a0376038f4fa3
NicTron <nicolasltroncoso@gmail.com>
2023-10-07 11:31:55 -0400

    Best price finder using keywords, pull user data from mongo

---
+++ mongo.py
@@ -0,0 +1,14 @@
+from pymongo.server_api import ServerApi
+from pymongo import MongoClient
+
+uri = "mongodb+srv://cluster0.4mp0egz.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority"
+client = MongoClient(uri,
+                     tls=True,
+                     tlsCertificateKeyFile='./cert.pem',
+                     server_api=ServerApi('1'))
+
+db = client['Knighthacks']
+collection = db['User Data']
+
+def userData(email):
+    return collection.find_one({"email":email})
---
+++ priceFind.py
@@ -0,0 +1,32 @@
+from requests_html import HTMLSession
+session = HTMLSession()
+
+def sort_by_price(array):
+  def get_price(obj):
+    return obj['price']
+
+  array.sort(key=get_price)
+  return array
+
+
+def getProducts(keywords):
+    queryString = ""
+    for item in keywords:
+        queryString = queryString + "+" + item
+    queryString = queryString[1:]
+    fullURL = 'https://www.google.com/search?q='+queryString+'&tbm=shop'
+    r = session.get(fullURL)
+    itemList = r.html.find('.sh-dgr__content')
+    finalList = []
+    for item in itemList:
+        links = list(item.find('.sh-dgr__offer-content')[0].absolute_links)
+        link = links[0]
+        price = item.search('${}.{}')[0]
+        finalList.append({
+            "link":link,
+            "price": int(price.replace(',', ''))
+        })
+    finalerList = sort_by_price(finalList[:10])
+    return finalerList
+
+print(getProducts(["macbook", "m1", "pro"]))
