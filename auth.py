dbfeb81236c4d02e6b617bbe2574dd1696318f23
NicTron <nicolasltroncoso@gmail.com>
2023-10-07 13:36:49 -0400

    Email OTP support

--- auth.py
+++ auth.py
@@ -1,15 +1,20 @@
 import smtplib
+import random
 from email.message import EmailMessage
 
-server = smtplib.SMTP("smtp.gmail.com", 587)
-server.starttls()
-server.login("troncosolnicolas@gmail.com", "vcivkqydcufkjbmm")
+def sendOTP(destination, otp):
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login("troncosolnicolas@gmail.com", "vcivkqydcufkjbmm")
 
-msg = EmailMessage()
-msg["Subject"] = "Test"
-msg["From"] = "Project <troncosolnicolas@gmail.com>"
-msg["To"] = "Nicolas Troncoso <nicolasltroncoso@gmail.com>"
-msg.set_content("Test email.")
+    msg = EmailMessage()
+    msg["Subject"] = "Test"
+    msg["From"] = "Authentication <troncosolnicolas@gmail.com>"
+    msg["To"] = destination
+    msg.set_content("Here is your one-time login code: " + otp)
 
-server.send_message(msg)
-server.quit()
+    server.send_message(msg)
+    server.quit()
+
+def genOTP():
+    return random.randint(10000, 99999)
